7c78bba787abedd5221cd9d4da6fc8642d822ed7
Gabriel Azzevedo <110872805+Grabium@users.noreply.github.com>
2024-04-02 02:22:29 +0000

    #3 - Página cole aqui com formulário construída com todas as suas variáveis de antes e depois da requisição assíncrona.

--- resources/views/body/windows/coleaqui.blade.php
+++ resources/views/body/windows/coleaqui.blade.php
@@ -1,33 +1,48 @@
 @extends('layouts/top')
 @section('content')
 
-<?php $fator = 0; ?>
+<?php 
+  $fator = (isset($fator)) ? $fator : 0 ; 
+  $fatorAntigo = (isset($fatorAntigo)) ? $fatorAntigo : "0" ;
+  $textoAntigoString = (isset($textoAntigoString)) ? $textoAntigoString : "!0!" ;
+?>
 
 <form action="{{ route('changed_chords') }}" method='POST'> 
-    @csrf
-    <div class="justify-content-center" >
-      <label for="fator" class="form-label">Alterar: </label>
-      <label class="form-label" id="lab_fator">0</label>
-      <label for="fator" class="form-label"> semi-tons.</label>
-    </div>
-    <div>
-      <input 
-        type="range" 
-        class="form-range" 
-        value="{{ $fator }}" 
-        id="fator" 
-        min="-11" max="11" step="1" 
-        onchange="slideValue(this.value)"  
-        oninput="slideValue(this.value)"/> <!-- em tempo real mas, não comatível com IE -->
-    </div>
+  @csrf
+  <div class="justify-content-center" >
+    <label for="fator" class="form-label">Alterar: </label>
+    <label class="form-label" id="lab_fator">0</label>
+    <label for="fator" class="form-label"> semi-tons.</label>
   </div>
+  
   <div>
-
-    <textarea id="texto" name="texto" rows="5" cols="33">
-Cole ou Escreva a música cifrada aqui.
-    </textarea>
-     
+    <input 
+      type="range" 
+      class="form-range" 
+      value="{{ $fator }}" 
+      id="fator" 
+      min="-11" max="11" step="1" 
+      onchange="slideValue(this.value)"  
+      oninput="slideValue(this.value)"/> <!-- em tempo real mas, não comatível com IE -->
   </div>
+
+  <div class='invisible'>
+    <input name="fator" id="fator" value="{{ $fator }}" /> 
+    <input name="fatorAntigo" id="fatorAntigo" value="{{ $fatorAntigo }}"/>
+    <textarea name="textoo" id="texto" rows="5" cols="60">{{ $textoAntigoString }}</textarea>
+  </div>
+    
+  
+  @if(isset($lines)) 
+    @foreach($lines as $line) 
+      <label id='lab_resultado' style="color:{{ ($line['cifer']) ? 'orange' : 'black' }}">{{ $line['content'] }}</label><br />
+    @endforeach 
+  @elseif($textoAntigoString == "!0!")
+    <div>
+      <textarea rows="5" cols="60">Cole ou digite a música cifrada aqui.</textarea>
+    </div>
+  @endif
+  
 </form>
 
 
